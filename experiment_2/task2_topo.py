90c0818a8c36cbf6007588906c396493606ce85a
xxion <monkey850416@gmail.com>
2020-10-08 22:33:04 +0800

    add exp2 inst.txt

--- experiment_2/task2_topo.py
+++ experiment_2/task2_topo.py
@@ -9,15 +9,13 @@ class MyTopo(Topo):
         h2 = self.addHost("h2")
         h3 = self.addHost("h3")
 		h4 = self.addHost("h4")
-
         s1 = self.addSwitch("s1")
         s2 = self.addSwitch("s2")
 
-
         self.addLink(h1,s1)
-        self.addLink(h2,s2)
-        self.addLink(h3,s2)
-        self.addLink(h3,s2)
+        self.addLink(h2,s1)
+        self.addLink(h3,s1)
         self.addLink(s1,s2)
+        self.addLink(h4,s2)
 
 topos = {"mytopo":(lambda : MyTopo())}
